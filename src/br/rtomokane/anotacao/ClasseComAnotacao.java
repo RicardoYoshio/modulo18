28f8601987146ecd6848bd401f2bc3a545135eb5
RicardoYoshio <rica.tomokane@gmail.com>
2023-05-03 15:03:22 -0300

    First Commit

---
+++ src/br/rtomokane/anotacao/ClasseComAnotacao.java
@@ -0,0 +1,13 @@
+package br.rtomokane.anotacao;
+
+@PrimeiraAnotacao(value = "Ricardo", bairros = "Teste", numeroCasa = 10)
+public class ClasseComAnotacao {
+
+    @PrimeiraAnotacao(value = "Tomo", bairros = {"Teste", "Teste1"}, numeroCasa = 20, valores = 100d)
+    private String nome;
+
+    @PrimeiraAnotacao(value = "Ricardo", bairros = "Teste", numeroCasa = 10)
+    public ClasseComAnotacao(){
+
+    }
+}
